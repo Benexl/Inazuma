3355a855413176a8e4fbf71216cc9edcdbb69a20
Benexl <benextempest@gmail.com>
2025-12-28 00:30:28 +0300

    feat: add settings logic

--- inazuma/__init__.py
+++ inazuma/__init__.py
@@ -1,3 +1,4 @@
+import json
 import os
 import random
 
@@ -8,12 +9,65 @@ from kivymd.app import MDApp
 from inazuma.view.screens import screens
 from inazuma.view.components.media_card.media_card import MediaPopup
 from kivy.logger import Logger
-
+from inazuma.utility.data import themes_available
 from typing import TYPE_CHECKING
+from kivy.uix.settings import SettingOptions
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.scrollview import ScrollView
+from kivy.uix.widget import Widget
+from kivy.uix.togglebutton import ToggleButton
+from kivy.uix.settings import SettingSpacer
+from kivy.uix.button import Button
+from kivy.metrics import dp
+from kivy.uix.popup import Popup
 
 if TYPE_CHECKING:
+    from kivy.uix.settings import Settings
     from viu_media.libs.media_api.types import MediaItem
     from viu_media.libs.provider.anime.types import Server
+
+#!python
+
+
+class SettingScrollOptions(SettingOptions):
+    def _create_popup(self, instance):
+        # global oORCA
+        # create the popup
+
+        content = GridLayout(cols=1, spacing="5dp")
+        scrollview = ScrollView(do_scroll_x=False)
+        scrollcontent = GridLayout(cols=1, spacing="5dp", size_hint=(None, None))
+        scrollcontent.bind(minimum_height=scrollcontent.setter("height")) # type: ignore
+        self.popup = popup = Popup(
+            content=content, title=self.title, size_hint=(0.5, 0.9), auto_dismiss=False
+        )
+
+        # we need to open the popup first to get the metrics
+        popup.open()
+        # Add some space on top
+        content.add_widget(Widget(size_hint_y=None, height=dp(2)))
+        # add all the options
+        uid = str(self.uid) # type: ignore
+        for option in self.options:
+            state = "down" if option == self.value else "normal"
+            btn = ToggleButton(
+                text=option,
+                state=state,
+                group=uid,
+                size=(popup.width, dp(55)),
+                size_hint=(None, None),
+            )
+            btn.bind(on_release=self._set_option) # type: ignore
+            scrollcontent.add_widget(btn)
+
+        # finally, add a cancel button to return on the previous panel
+        scrollview.add_widget(scrollcontent)
+        content.add_widget(scrollview)
+        content.add_widget(SettingSpacer())
+        # btn = Button(text='Cancel', size=((oORCA.iAppWidth/2)-sp(25), dp(50)),size_hint=(None, None))
+        btn = Button(text="Cancel", size=(popup.width, dp(50)), size_hint=(0.9, None))
+        btn.bind(on_release=popup.dismiss) # type: ignore
+        content.add_widget(btn)
 
 
 class Inazuma(MDApp):
@@ -47,12 +101,12 @@ class Inazuma(MDApp):
 
         self.generate_application_screens()
 
-        # if config := self.config:
-        #     if theme_color := config.get("Preferences", "theme_color"):
-        #         self.theme_cls.primary_palette = theme_color
-        #     if theme_style := config.get("Preferences", "theme_style"):
-        #         self.theme_cls.theme_style = theme_style
-        #
+        if config := self.config:
+            if theme_color := config.get("Preferences", "theme_color"):
+                self.theme_cls.primary_palette = theme_color
+            if theme_style := config.get("Preferences", "theme_style"):
+                self.theme_cls.theme_style = theme_style
+
         # self.anime_screen = self.manager_screens.get_screen("anime screen")
         # self.search_screen = self.manager_screens.get_screen("search screen")
         # self.download_screen = self.manager_screens.get_screen("downloads screen")
@@ -61,6 +115,64 @@ class Inazuma(MDApp):
 
     def on_start(self, *args):
         self.media_card_popup = MediaPopup()
+
+    def build_config(self, config):
+        # General settings setup
+        config.setdefaults(
+            "Preferences",
+            {
+                "theme_color": "Cyan",
+                "theme_style": "Dark",
+                "downloads_dir": self.viu.config.downloads.downloads_dir,
+            },
+        )
+
+    def build_settings(self, settings: "Settings"):
+        settings.register_type("scrolloptions", SettingScrollOptions)
+        app_settings = [
+            {"type": "title", "title": "Preferences"},
+            {
+                "type": "scrolloptions",
+                "title": "Theme Color",
+                "desc": "Sets the theme color to be used for the app for more info on valid theme names refer to help",
+                "section": "Preferences",
+                "key": "theme_color",
+                "options": themes_available,
+            },
+            {
+                "type": "options",
+                "title": "Theme Style",
+                "desc": "Sets the app to dark or light theme",
+                "section": "Preferences",
+                "key": "theme_style",
+                "options": ["Light", "Dark"],
+            },
+            {
+                "type": "path",
+                "title": "Downloads Directory",
+                "desc": "location to download your videos",
+                "section": "Preferences",
+                "key": "downloads_dir",
+            },
+        ]
+
+        settings.add_json_panel("Inazuma Settings", self.config, data=json.dumps(app_settings))
+
+    def on_config_change(self, config, section, key, value):
+        # TODO: Change to match case
+        if section == "Preferences":
+            match key:
+                case "theme_color":
+                    if value in themes_available:
+                        self.theme_cls.primary_palette = value
+                    else:
+                        Logger.warning(
+                            "AniXStream Settings: An invalid theme has been entered and will be ignored"
+                        )
+                        config.set("Preferences", "theme_color", "Cyan")
+                        config.write()
+                case "theme_style":
+                    self.theme_cls.theme_style = value
 
     def generate_application_screens(self) -> None:
         for i, name_screen in enumerate(screens.keys()):
@@ -198,40 +310,6 @@ class Inazuma(MDApp):
             if task_id in self.active_downloads:
                 del self.active_downloads[task_id]
 
-    #
-    # def build_config(self, config):
-    #     # General settings setup
-    #     config.setdefaults(
-    #         "Preferences",
-    #         {
-    #             "theme_color": "Cyan",
-    #             "theme_style": "Dark",
-    #             "downloads_dir": USER_VIDEOS_DIR,
-    #         },
-    #     )
-    #
-    # def build_settings(self, settings: Settings):
-    #     settings.add_json_panel(
-    #         "Settings", self.config, resource_find("general_settings_panel.json")
-    #     )
-    #
-    # def on_config_change(self, config, section, key, value):
-    #     # TODO: Change to match case
-    #     if section == "Preferences":
-    #         match key:
-    #             case "theme_color":
-    #                 pass
-    #                 # if value in themes_available:
-    #                 #     self.theme_cls.primary_palette = value
-    #                 # else:
-    #                 #     Logger.warning(
-    #                 #         "AniXStream Settings: An invalid theme has been entered and will be ignored"
-    #                 #     )
-    #                 #     config.set("Preferences", "theme_color", "Cyan")
-    #                 #     config.write()
-    #             case "theme_style":
-    #                 self.theme_cls.theme_style = value
-    #
     # def on_stop(self):
     #     pass
     #
